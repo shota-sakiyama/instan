271544a6716e3eaf57040765585a07deb3bde7be
shota <#shota.gad1@gmail.com>
2021-02-15 01:56:14 +0900

    ログイン機能一部修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user
     if @current_user == nil
-      flash[:notice] = t('notice.login_needed')
+      flash[:notice] = "ログインが必要です。"
       redirect_to new_session_path
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,17 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user.id)
+      session[:user_id] = @user.id
     else
       render :new
     end
   end
+
   def show
     @user = User.find(params[:id])
   end
+
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,:password_confirmation)
