91c8236142e73ae79ead4c95c2f436d1291f12d3
shota <#shota.gad1@gmail.com>
2021-02-18 00:03:10 +0900

    投稿機能追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user, {only: [:new, :create, :edit, :confirm, :update, :show, :destroy]}
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def confirm
+    @post = current_user.posts.build(post_params)
+    render :new if @post.invalid?
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to posts_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @post = current_user.posts.build(post_params)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   get 'sessions/new'
   resources :users
   resources :sessions
+  resources :posts do
+    collection do   
+      post :confirm
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
