40134845948c3b65f5f84dd2c4ad5b5c2ce1c48c
shota <#shota.gad1@gmail.com>
2021-02-18 01:29:55 +0900

    日本語設定

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,16 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Instan
+module Instantgram
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
