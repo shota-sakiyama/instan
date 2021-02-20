a0ff6e31ae446b501e4689717e3685a3c4dc7b56
shota <#shota.gad1@gmail.com>
2021-02-20 22:57:11 +0900

    国際化重複削除

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Instantgram
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators do |g|
       g.assets false
