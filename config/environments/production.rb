93f54011a58ccb0c617e258f98b67dc2f2fcf973
shota <#shota.gad1@gmail.com>
2021-02-21 13:10:53 +0900

    メール機能再修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,19 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'calm-caverns-66778.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "instan_#{Rails.env}"
